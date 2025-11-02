f27e9136040ef25b03595032202395495440fe8d
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-02 18:20:53 +0200

    Changed admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year')
+
+admin.site.register(Book, BookAdmin)
