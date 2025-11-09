7cdd25625d684e5b36cafb06f137effb2077178e
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-09 20:54:10 +0200

    Fixed logic for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,7 +10,7 @@ from .models import Book
 
 # Create your views here.
 class RegisterView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
