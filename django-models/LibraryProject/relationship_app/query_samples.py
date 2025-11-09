4204579b5ba29ead049fbb787ca5e566f8788023
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-09 15:55:03 +0200

    Fixed listing query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,8 @@ author = Author.objects.get(name="J.K. Rowling")
 books_by_author = Book.objects.filter(author=author)
 
 #List all the books of a library
-library = Library.objects.get(name="City Library")
+library_name = "City Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
 #Retrieve the librarian for a library
