491fa1205156a1374e20d60fe97dc5441fbe8e05
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-09 22:12:13 +0200

    Fixed logic for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test, permission_required, login_required
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
