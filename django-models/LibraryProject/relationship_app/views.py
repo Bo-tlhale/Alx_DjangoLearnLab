461440c89df125a9f58ca5738aeb25cb214e7648
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-09 20:12:12 +0200

    Fixed logic for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,11 @@ from .models import Book, Library
 def get_all_books(request):
     books = Book.objects.all()
     context = {'list_of_books': books}
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetailsView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
