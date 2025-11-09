ebe0df5b8df1117e83dde595400a375f983543c5
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-09 20:16:27 +0200

    Fixed logic for checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', views.get_all_books, name='all_books'),
-    path('library/<int:pk>/', views.LibraryDetailsView.as_view(), name='library_books'),
+    path('books/', list_books, name='all_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_books'),
 ]
