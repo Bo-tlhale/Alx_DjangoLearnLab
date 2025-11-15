85835ea37fde5ae3c45c9d99896a1e9244b3b693
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-15 14:16:43 +0200

    Created permissions and their groups

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractUser, Group, Permission
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
 #Models
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
@@ -27,3 +30,33 @@ class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
     publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can Create"),
+            ("can_edit", "Can Edit"),
+            ("can_view", "Can View"),
+            ("can_delete", "Can Delete"),
+        ]
+
+@receiver(post_migrate)
+def create_user_groups(sender, **kwargs):
+    if sender.name != 'bookshelf':
+        return
+    
+    admins, created = Group.objects.get_or_create(name='Admins')
+    editors, created = Group.objects.get_or_create(name='Editors')
+    viewers, created = Group.objects.get_or_create(name='Viewers')
+
+    try:
+        can_create = Permission.objects.get(codename='can_create')
+        can_edit = Permission.objects.get(codename='can_edit')
+        can_view = Permission.objects.get(codename='can_view')
+        can_delete = Permission.objects.get(codename='can_delete')
+    except Permission.DoesNotExist:
+        return
+    
+    admins.permissions.set([can_create, can_edit, can_view, can_delete])
+    editors.permissions.set([can_create, can_edit, can_view])
+    viewers.permissions.set([can_view])
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+@permission_required('bookshef.can_create', raise_exception=True)
+def create_book():
+    return
+
+@permission_required('bookshef.can_edit', raise_exception=True)
+def edit_book():
+    return
+
+@permission_required('bookshef.can_view', raise_exception=True)
+def view_book():
+    return
+
+@permission_required('bookshef.can_delete', raise_exception=True)
+def delete_book():
+    return
