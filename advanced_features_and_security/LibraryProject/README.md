07907acf183c4349154b5db6344f4573d69ebd34
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-15 14:22:53 +0200

    Fixed logic for checker

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -1,1 +1,6 @@
 My first Django Web App
+
+--Groups and their permissions
+#Admins can create, view, edit and delete
+#Editors can create, view, edit and delete
+#Viewers can only view
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 # Create your views here.
 @permission_required('bookshef.can_create', raise_exception=True)
@@ -17,3 +18,9 @@ def view_book():
 @permission_required('bookshef.can_delete', raise_exception=True)
 def delete_book():
     return
+
+@permission_required('bookshef.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    context = {'list_of_books': books}
+    return render(request, '/book_list.html', context)
